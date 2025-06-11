ea50d709ff27db27b10a477406213c0f1a247c07
auvrox61 <auvro61@gmail.com>
2025-06-11 21:53:45 +0600

    Add files via upload

---
+++ basic_variables.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age=25;
+    char grade='A';
+    float cgpa=3.44;
+    bool pass_status=true;
+    cout<<"Age : "<<age<<endl;
+    cout<<"Grade : "<<grade<<"-"<<endl;
+    cout<<"CGPA : "<<cgpa<<endl;
+    cout<<"Pass Status(1 for pass,0 for fail) : "<<pass_status<<endl;
+    return 0;
+}
